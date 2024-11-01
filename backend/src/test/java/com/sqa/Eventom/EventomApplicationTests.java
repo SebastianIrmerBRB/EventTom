93a12dce6d9c12b6b840f5779000fadaccad0cc4
basti <irmersebastian@web.de>
2024-11-01 08:02:05 +0100

    Init

---
+++ backend/src/test/java/com/sqa/Eventom/EventomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqa.Eventom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
